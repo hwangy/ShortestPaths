877b75013a7472299bc95726e395a6f317ad10ae
Cassandra <cassandramarcussencello@gmail.com>
2023-04-25 15:12:41 -0400

    Adding tests

---
+++ src/test/java/DistributedRouting/AgentControllerTest.java
@@ -0,0 +1,31 @@
+package DistributedRouting;
+
+import DistributedRouting.grpc.CouponMessageRequest;
+import DistributedRouting.util.TestObjects;
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
+
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.withSettings;
+
+public class AgentControllerTest {
+
+    @Test 
+    void testInitializeListener() {
+
+        
+    }
+
+    @Test
+    void testDrawGraph() {
+
+        
+    }
+
+}
--- src/test/java/DistributedRouting/MessageServerTest.java
+++ src/test/java/DistributedRouting/MessageServerTest.java
@@ -3,6 +3,8 @@ package DistributedRouting;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import DistributedRouting.grpc.MessageRequest;
+import DistributedRouting.grpc.BFSMessageRequest;
+import DistributedRouting.grpc.CouponMessageRequest;
 
 /**
  * Unit tests for the MessageServer class. These unit tests encompass the various functionalities of 
@@ -11,4 +13,44 @@ import DistributedRouting.grpc.MessageRequest;
  */
 class MessageServerTest {
 
+    @Test
+    void testMessageRequest() {
+        MessageRequest message = MessageRequest.newBuilder()
+            .setNodeId(1)
+            .build();
+
+        Assertions.assertTrue(message.getNodeId() == 1);
+    }
+
+    @Test
+    void testBFSMessageRequest() {
+        BFSMessageRequest message = BFSMessageRequest.newBuilder()
+            .setOriginId(1)
+            .setParentId(2)
+            .setLevel(5)
+            .build();
+
+        Assertions.assertTrue(message.getOriginId() == 1);
+        Assertions.assertTrue(message.getParentId() == 2);
+        Assertions.assertTrue(message.getLevel() == 5);
+    }
+
+    @Test
+    void testCouponMessageRequest() {
+        CouponMessageRequest message = CouponMessageRequest.newBuilder()
+            .setForward(true)
+            .setOriginId(1)
+            .setParentId(2)
+            .setCurrentWalkLength(10)
+            .addFullWalk(11)
+            .setWeight(1)
+            .build();
+        
+        Assertions.assertTrue(message.getForward() == true);
+        Assertions.assertTrue(message.getOriginId() == 1);
+        Assertions.assertTrue(message.getParentId() == 2);
+        Assertions.assertTrue(message.getCurrentWalkLength() == 10);
+        //Assertions.assertTrue(message.getFullWalk().contains(11));
+        Assertions.assertTrue(message.getWeight() == 1);
+    }
 }
